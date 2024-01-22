61e78d517a6f917ce894362f36ec9af5fbd5a0a0
HoussemDAAS <houssemdaas2@gmail.com>
2024-01-22 14:41:10 +0100

    first time adding Router in Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import { block } from 'million'
 import './App.css'
 import Nav from './components/NavBar/Navbar';
 import Home from './components/NavBar/Home/Home';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const  App=block(() => {
 
 
   return (
+    <Router>
     <div className='w-full mx-auto pt-0'>
    <Nav />
-<Home/>
+   <Routes>
+    <Route path='/' element={<Home />} />
+   </Routes>
+
     </div>
+    </Router>
   )
 });
 
