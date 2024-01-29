214f3a8545d520ff14df2ce346f9cb4adeeff98d
HoussemDAAS <houssemdaas2@gmail.com>
2024-01-29 15:23:51 +0100

    fetching data

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import contents from'../../Content/data'
 import { Link } from 'react-router-dom'
 
+
 const Article = () => {
     const {name} = useParams();
     const blog =contents.find(blog => blog.name === name);
+const [articleInfo, setArticleInfo] = useState({comments:[]});
+
+useEffect(() => {
+  const fetchData = async () => {
+    const result = await fetch(`http://localhost:3001/api/articles/${name}`);
+
+    const body= await result.json();
+    console.log(body);
+    setArticleInfo(body);
+  };
+  fetchData();
+},[name]);
+
  if(!blog){
    return <h1>Blog Not Found</h1>
  }
    
   return (
-<div className='w-full  column items-center m-6 align-center my-6'>
-        <h1 className='text-5xl font-sans items-center my-6 text-center text-gray-600 font-bold-bold' >{blog.title}</h1>
-        <p className='text-xl text-center m-4 text-gray-400'>{blog.content.map((paragraph,index) => 
-            <p key={index}>{paragraph}</p>)}</p>
-            <Link
-            to ="/articles">
-                  <h1 className='sm:text-2xl text-xl font-sans items-center my-6 text-center text-gray-600 font-bold-bold'>other articles</h1>
-            </Link>
-
-      
-    </div>
-  )
-}
+<div className='w-full column items-center m-6 align-center my-6'>
+  <h1 className='text-5xl font-sans items-center my-6 text-center text-gray-600 font-bold-bold'>{blog.title}</h1>
+  <div className='text-xl text-center m-4 text-gray-400'>
+    {blog.content.map((paragraph, index) => (
+      <p key={index}>{paragraph}</p>
+    ))}
+  </div>
+  <Link to="/articles">
+    <h1 className='sm:text-2xl text-xl font-sans items-center my-6 text-center text-gray-600 font-bold-bold'>other articles</h1>
+  </Link>
+</div>
+  )};
 
 export default Article
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -3,24 +3,24 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <div>
-     <div class="min-h-full">
-  <nav class="bg-gray-800">
-    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div class="flex h-16 items-center justify-between">
-        <div class="flex items-center">
-          <div class="flex-shrink-0">
+     <div className="min-h-full">
+  <nav className="bg-gray-800">
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="flex h-16 items-center justify-between">
+        <div className="flex items-center">
+          <div className="flex-shrink-0">
          
           </div>
       
-            <div class="ml-10 flex place-items-end space-x-4">
+            <div className="ml-10 flex place-items-end space-x-4">
     
             <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Home</Link>
             <Link to="/blogs" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Blog List</Link>
             <Link to="/blog" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Blog</Link>
             <Link to="/about" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About Us</Link>
             
-              {/* <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
-              <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Reports</a> */}
+              {/* <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Reports</a> */}
             </div>
         
         </div>
